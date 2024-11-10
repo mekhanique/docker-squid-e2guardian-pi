c1a1bf9f02dfc63e1da0b983c14c5a5f918a7f98
Steve <mekhanique@users.noreply.github.com>
2024-11-10 12:49:07 -0800

    Update TLS Key to use 4096 bit encryption

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ SQUID_USER=squid
 SQUID_DIR=/usr/local/squid
 
 if [ ! -f $SQUID_DIR/ssl/bluestar.pem ]; then
-    openssl req -new -newkey rsa:2048 -nodes -days 3650 -x509 -keyout $SQUID_DIR/ssl/bluestar.pem -out $SQUID_DIR/ssl/bluestar.crt\
+    openssl req -new -newkey rsa:4096 -nodes -days 3650 -x509 -keyout $SQUID_DIR/ssl/bluestar.pem -out $SQUID_DIR/ssl/bluestar.crt\
 	    -subj "/C=US/ST=Texas/L=Austin/O=BlueStar/OU=NetworkSecurity/CN=bluestar"
     openssl x509 -in $SQUID_DIR/ssl/bluestar.crt -outform DER -out $SQUID_DIR/ssl/bluestar.der
 fi
